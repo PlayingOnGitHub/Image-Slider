e05094fc53b232d392e4c39f6165768912f09429
PlayingOnGitHub <gobertja@vcu.edu>
2020-09-27 01:36:27 -0400

    Creating functions that shift an 'image-array' to the correct location, based on which arrow is clicked. I'm also creating functions which will serve as a blueprint for now. I will later consider how to actually animate the images

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const imageArray = ["controller-1", "controller-2", "controller-3"];
+let currentItem = 0;
+
+function changeArray() {
+  const arrow = this;
+  const direction = arrow.id;
+  if (direction === "left-arrow") {
+    moveToTheRight();
+  } else if (direction === "right-arrow") {
+    moveToTheLeft();
+  }
+}
+
+function moveImages() {
+  const arrow = this;
+}
+
+function shiftItems() {
+  changeArray.call(this);
+  moveImages.call(this);
+  console.log(currentItem);
+}
+
+function moveToTheLeft() {
+  /* if we are at the last array item */
+  if (currentItem === imageArray.length - 1) {
+    currentItem = 0;
+    return;
+  }
+  currentItem += 1;
+}
+
+function moveToTheRight() {
+  /* if we are at the start of the array, shift
+     to the end of the array */
+  if (currentItem === 0) {
+    currentItem = imageArray.length - 1;
+    return;
+  }
+  currentItem -= 1;
+}
+
+/* test functions */
+function moveLeft() {
+  const leftArrow = document.getElementById("left-arrow");
+  shiftItems.call(leftArrow);
+}
+
+function moveRight() {
+  const rightArrow = document.getElementById("right-arrow");
+  shiftItems.call(rightArrow);
+}
